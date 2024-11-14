985a69f7f7fd44b2db8645d7985aef6181634a3b
Prachi Rajput <prachiraj9149@gmail.com>
2024-11-14 13:01:36 +0530

    Add files via upload

---
+++ Flight.java
@@ -0,0 +1,11 @@
+public class Flight {
+    private int flightID;
+    private String flightNumber;
+    private String origin;
+    private String destinaton;
+    private String date;
+    private String time;
+    private int seatsAvailable;
+
+    //constructors, getters, and setters
+}
